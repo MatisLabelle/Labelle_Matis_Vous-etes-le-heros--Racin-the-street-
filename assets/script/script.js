270bd2187c7d133770491d30027a35d6bf656315
MatisLabelle <mat48337@gmail.com>
2023-11-03 11:52:19 -0400

    ajustement des image

--- assets/script/script.js
+++ assets/script/script.js
@@ -74,16 +74,19 @@ const chapters = {
         destination: "defaite",
       },
     ],
+    video: "assets/images/car-meet.mp4",
   },
   victoire: {
     titre: "Victoire",
     description:
       "Bravo ! Vous avez gagnez la course contre la mustang. Vous êtes maintenant un membre respecté de la communauté.",
     image: "assets/images/victoire.png",
+    video: "assets/images/win.mp4",
   },
   defaite: {
     titre: "Défaite",
-    description: "Vous avez perdu la course.",
+    description:
+      "Vous avez perdu la course. Votre voiture n'était pas assez rapide.",
     image: "assets/images/defaite.png",
     boutons: [
       {
@@ -95,6 +98,7 @@ const chapters = {
 };
 
 //Récupère les éléments du HTML
+let container = document.getElementById("jeu");
 let titreChapitre = document.getElementById("chapitre");
 let textChapitre = document.getElementById("text");
 let imageChapitre = document.getElementById("image-album");
@@ -103,49 +107,109 @@ let btns = document.querySelector(".btn-pg1");
 // met la twist a false
 let twist = false;
 
+// creation du son
+const vroum = new Audio("assets/sons/mixkit-fast-car-drive-by-1538.wav");
+
 // function de changement de chapitre
 function goToChapter(chapitreKey) {
+  if (vroum.paused) {
+    vroum.play();
+  } else {
+    vroum.currentTime = 0;
+    vroum.play();
+  }
+
   if (chapters[chapitreKey]) {
     const chapitre = chapters[chapitreKey];
 
-    // change l'interface pour le nouveau chapitre
-    titreChapitre.textContent = chapitre.titre;
-    textChapitre.textContent = chapitre.description;
-    imageChapitre.src = chapitre.image;
-
-    // supprime les vieux boutons
-    while (btns.firstChild) {
-      btns.removeChild(btns.firstChild);
+    let media = document.querySelector(".media");
+    if (media) {
+      container.removeChild(media);
     }
 
-    // créer les nouveaux boutons
-    for (let i = 0; i < chapitre.boutons.length; i++) {
-      let btn = document.createElement("button");
-      btn.classList.add("btn");
-      btn.textContent = chapitre.boutons[i].titre;
-      btns.appendChild(btn);
+    // change l'interface pour le nouveau chapitre
+    if (chapitre.video) {
+      let video = document.createElement("video");
+      video.src = chapitre.video;
+      video.classList.add("media");
+      container.appendChild(video);
+      video.play();
+      video.volume = 0;
+      titreChapitre.textContent = chapitre.titre;
+      textChapitre.textContent = chapitre.description;
 
-      // si la twist est true, et qu'on fait la course au chapitre meet, nous envoie à la victoire
-      btn.addEventListener("click", function () {
-        if (
-          chapitreKey === "meet" &&
-          chapitre.boutons[i].titre === "Oui" &&
-          twist == true
-        ) {
-          goToChapter("victoire");
-        } else {
-          goToChapter(chapitre.boutons[i].destination);
-        }
-      });
+      while (btns.firstChild) {
+        btns.removeChild(btns.firstChild);
+      }
+
+      // créer les nouveaux boutons
+      for (let i = 0; i < chapitre.boutons.length; i++) {
+        let btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.textContent = chapitre.boutons[i].titre;
+        btns.appendChild(btn);
+
+        // si la twist est true, et qu'on fait la course au chapitre meet, nous envoie à la victoire
+        btn.addEventListener("click", function () {
+          if (
+            chapitreKey === "meet" &&
+            chapitre.boutons[i].titre === "Oui" &&
+            twist == true
+          ) {
+            goToChapter("victoire");
+          } else {
+            goToChapter(chapitre.boutons[i].destination);
+          }
+        });
+      }
 
       // met la twist à true si on choisit suspension et échappement
       if (
         chapitreKey === "garage" &&
         chapitre.boutons[i].titre === "Suspension et échappement"
       ) {
+        twist = true;
+      }
+    } else {
+      container.appendChild(imageChapitre);
+      imageChapitre.classList.add("media");
+      titreChapitre.textContent = chapitre.titre;
+      textChapitre.textContent = chapitre.description;
+      imageChapitre.src = chapitre.image;
+
+      while (btns.firstChild) {
+        btns.removeChild(btns.firstChild);
+      }
+
+      // créer les nouveaux boutons
+      for (let i = 0; i < chapitre.boutons.length; i++) {
+        let btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.textContent = chapitre.boutons[i].titre;
+        btns.appendChild(btn);
+
+        // si la twist est true, et qu'on fait la course au chapitre meet, nous envoie à la victoire
         btn.addEventListener("click", function () {
-          twist = true;
+          if (
+            chapitreKey === "meet" &&
+            chapitre.boutons[i].titre === "Oui" &&
+            twist == true
+          ) {
+            goToChapter("victoire");
+          } else {
+            goToChapter(chapitre.boutons[i].destination);
+          }
         });
+
+        // met la twist à true si on choisit suspension et échappement
+        if (
+          chapitreKey === "garage" &&
+          chapitre.boutons[i].titre === "Suspension et échappement"
+        ) {
+          btn.addEventListener("click", function () {
+            twist = true;
+          });
+        }
       }
     }
 
